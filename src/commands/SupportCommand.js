fcfb7f1cd4f2348c879c69f2432bae7f92861e99
Riode <88613092+ri0de@users.noreply.github.com>
2021-08-08 19:10:16 +0600

    initial commit

---
+++ src/commands/SupportCommand.js
@@ -0,0 +1,47 @@
+const Command = require("../Command");
+
+class SupportCommand {
+    constructor(client) {
+        super(client, {
+            name: ["support", "server"]
+        });
+    };
+
+     /**
+     * @param {Message} message
+     */
+      async execute (message) {
+
+        return message.channel.send({
+            embeds: [
+                {
+                    title: "Support",
+                    description: "Click on the button or this [HyperText]" + `(https://discord.gg/VgK2dar8Sg)`,
+                    color: "#dddddd",
+                    thumbnail: {
+                        url: String(this.client.user.displayAvatarURL())
+                    },
+                    footer: {
+                        text: String(message.member.displayName),
+                        iconURL: String(message.author.displayAvatarURL({ dynamic: true}))
+                    }
+                }
+            ],
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            style: 5,
+                            url: "https://discord.gg/VgK2dar8Sg",
+                            label: "Join us"
+                        }
+                    ]
+                }
+            ]
+        });
+    };
+};
+
+module.exports = SupportCommand;
